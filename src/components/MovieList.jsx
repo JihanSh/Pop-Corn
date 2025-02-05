9db52337d4fca5abefe21f64cdd97f2037ba9cfb
Jihan Shamas <jihan.shamas@gmail.com>
2025-02-05 14:25:06 +0100

    update the functionality to display the list of movies and the functionality to delete them

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import movies from "../data/movies.json";
 import "./MovieList.css";
+
 function MovieList() {
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+  const deleteMovie = (movieId) => {
+    console.log("deleting movie with the id...", movieId);
+    const newListOfMovies = moviesToDisplay.filter((movie) => {
+      if (movie.id !== movieId) {
+        return true;
+      } else return false;
+    });
+    setMoviesToDisplay(newListOfMovies);
+  };
+
   return (
     <div className="MovieList">
-      <h2>List Of Movies </h2>
-      {movies.map((movie) => {
+      <h2>List Of Movies {moviesToDisplay.length} </h2>
+      {moviesToDisplay.map((movie) => {
         return (
           <div key={movie.id} className="card">
             <p>Title: {movie.title}</p>
-            <img src={movie.imgURL} />
+            <img src={movie.imgURL} alt={movie.title} />
             <p>Rating: {movie.rating}</p>
+            <button
+              onClick={() => {
+                deleteMovie(movie.id);
+              }}
+            >
+              Delete this movie
+            </button>
           </div>
         );
       })}
